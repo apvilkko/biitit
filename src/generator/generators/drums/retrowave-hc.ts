715684b6964a4b127933dac026f3bf95e164efd1
Antti Pekka Vilkko <apvilkko@gmail.com>
2023-01-23 15:01:15 +0200

    feat: default retrowave hc style

--- src/generator/generators/drums/retrowave-hc.ts
+++ src/generator/generators/drums/retrowave-hc.ts
@@ -1,5 +1,5 @@
 import { createDrumGenerator } from '../utils'
-import { randLt, randFloat, rand } from '../../../utils'
+import { randLt, randFloat, rand, sample } from '../../../utils'
 import { NOTE_LENGTH } from '../../constants'
 import { GeneratorInterface, Scene } from '../../../types'
 import instruments from '../../instruments'
@@ -7,43 +7,18 @@ const { sixteenth, eighth, quarter } = NOTE_LENGTH
 
 const { HC } = instruments
 
-/*
-quarter,
-    ({ scene }) => {
-      const leaveOut = rand(0, 3);
-      const choices = [0, 1, 2, 3].filter(x => x !== leaveOut);
-      const spec = scene.instruments[DRUMS].specs[HC];
-      const common = { instrument: HC, note: spec.pitch };
-      return { choices, spec, common };
-    },
-    ({ currentNote, data: { spec, common, choices } }) => {
-      const condition =
-        spec.style === "8th"
-          ? currentNote % eighth === 0
-          : spec.style === "16th"
-          ? currentNote % sixteenth === 0
-          : spec.style === "three"
-          ? choices
-              .map(x => currentNote % quarter === x * sixteenth)
-              .some(x => !!x)
-          : false;
-      if (condition) {
-        return { ...common, velocity: spec.volume };
-      }
-      return null;
-    }
-*/
+export const retrowaveHc: GeneratorInterface = (opts) => {
+  const hhStyle = opts.style ?? sample(['8th', '16th', 'three'])
 
-export const retrowaveHc: GeneratorInterface = (opts) =>
-  createDrumGenerator(
+  return createDrumGenerator(
     opts,
     ({ currentNote, spec, common, data: { choices } }) => {
       const condition =
-        opts.style === '8th'
+        hhStyle === '8th'
           ? currentNote % eighth === 0
-          : opts.style === '16th'
+          : hhStyle === '16th'
           ? currentNote % sixteenth === 0
-          : opts.style === 'three'
+          : hhStyle === 'three'
           ? choices
               .map((x) => currentNote % quarter === x * sixteenth)
               .some((x) => !!x)
@@ -62,3 +37,4 @@ export const retrowaveHc: GeneratorInterface = (opts) =>
       return { choices, spec, common }
     }
   )
+}
